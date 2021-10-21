f9ef971a066d1e2b5229713aa43fbe5ead2b63a0
erikacamilleri <erika.camilleri@gmail.com>
2021-10-21 08:29:44 +0200

    First program Hello, World

--- Year9/HelloWorld/HelloWorld.java
+++ Year9/HelloWorld/HelloWorld.java
@@ -1,9 +1,9 @@
 
 /**
- * Write a description of class HelloWorld here.
+ * The first program that we write in Java
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Erika Camilleri
+ * @version 0.1
  */
 public class HelloWorld
 {
