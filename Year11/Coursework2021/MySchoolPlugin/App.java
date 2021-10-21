de11e12ad5f2eff7790baac03515bf5b3df36093
erikacamilleri <erika.camilleri@gmail.com>
2021-10-21 15:24:17 +0200

    How to set up project for Coursework

---
+++ Year11/Coursework2021/MySchoolPlugin/App.java
@@ -0,0 +1,13 @@
+
+/**
+ * This is the entry point of my app
+ *
+ * @author Erika Camilleri
+ * @version 0.1
+ */
+public class App
+{
+    public static void main(String[] args) {
+    
+    }
+}
---
+++ Year11/Coursework2021/MySchoolPlugin/MySchoolPlugin.java
@@ -0,0 +1,11 @@
+
+/**
+ * A plugin for disciplinary suggestions in myschool platform.
+ *
+ * @author Erika Camilleri
+ * @version 0.1
+ */
+public class MySchoolPlugin
+{
+    
+}
